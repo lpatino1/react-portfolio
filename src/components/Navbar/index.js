49f3e7dd4b412f7e25c226be3e5554fa190335bb
Lesli <lpatino0994@gmail.com>
2022-08-29 00:23:16 -0500

    created react app, added nav bar and header components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import "../Header/index.js";
+import Header from "../Header/index.js";
+
+export default function Navbar() {
+    return (
+      <div className="nav">
+        <Header/>
+      </div>
+    );
+  }
+  
+  
